1b5d6a5348833c9f41f2c4765fd017e6c85f8510
Mahmoud Maher <mahmoudmaherfcih30051994@gmail.com>
2023-11-09 10:39:35 +0200

    dashboard styles

---
+++ resources/views/dashboard/sliders/partials/actions/create.blade.php
@@ -0,0 +1,4 @@
+
+<a href="{{ route('sliders.create') }}" class="btn btn-primary">
+    {{ __('sliders.actions.create') }} <i class="mdi mdi-plus"></i>
+</a>
---
+++ resources/views/dashboard/sliders/partials/actions/edit.blade.php
@@ -0,0 +1,1 @@
+<a href="{{ route('sliders.edit', $slider) }}" class="btn btn-sm btn-info btn-b"> <i class="las la-pen"></i> </a> 
